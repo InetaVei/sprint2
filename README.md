0befabb8deac028b389d9f3056d56874a5ec2808
InetaVei <74532995+InetaVei@users.noreply.github.com>
2021-03-15 14:40:22 +0200

    html part

---
+++ README.md
@@ -0,0 +1,14 @@
+![Awesome](https://cdn.rawgit.com/sindresorhus/awesome/d7305f38d29fed78fa85652e3a63e154dd8e8829/media/badge.svg)
+
+# Sprint2
+### PHP Files Browser Demo
+
+This project is for my educational purposes.
+
+
+## Project features
+
+
+
+## Authors
+[Ineta](https://github.com/InetaVei)
---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <title>Sprint2</title>
+</head>
+<body>
+<div class="container-fluid">
+  <div class="row">
+      <div class="col-12">
+      
+          <div class="btn-group" role="group" aria-label="Basic example">
+            <button type="button" name="submit" class="btn btn-primary" style="background-color: lightblue; color:black; border-color:black">Darbuotojas</button>
+            <button type="button" name="submit" class="btn btn-primary" style="background-color: lightblue; color:black; border-color:black">Projektas</button>
+          </div>
+
+<table class="table table-striped table-hover">
+
+  <thead style="background-color: lightblue">
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">Vardas Pavardė</th>
+      <th scope="col">Projektas</th>
+      <th scope="col">Veiksmas</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+  </tbody>
+</table>
+
+<div class="container">
+    <div class="row">
+      <div class="col-3">
+          <form action="" method="post">    
+            <input type="name" name="name" class="form-control" id="name" placeholder="Naujas projekto pavadinimas">
+      </div>
+        <div class="col-2">
+            <button type="button" name="submit" class="btn btn-primary" style="background-color: lightblue; color:black; border-color:black">Pridėti projektą</button>
+        </div>
+        </form>
+    </div>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
