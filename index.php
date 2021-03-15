c8224d4129fe654aa8f5a6c6c2e0eb22c33cff44
InetaVei <74532995+InetaVei@users.noreply.github.com>
2021-03-15 15:00:22 +0200

    html

--- index.php
+++ index.php
@@ -6,11 +6,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  
   <title>Sprint2</title>
 </head>
 <body>
-<div class="container-fluid">
-  <div class="row">
+  <div class="container-fluid">
+    <div class="row">
       <div class="col-12">
       
           <div class="btn-group" role="group" aria-label="Basic example">
@@ -18,42 +19,46 @@
             <button type="button" name="submit" class="btn btn-primary" style="background-color: lightblue; color:black; border-color:black">Projektas</button>
           </div>
 
-<table class="table table-striped table-hover">
+  <table class="table table-striped table-hover">
 
-  <thead style="background-color: lightblue">
-    <tr>
-      <th scope="col">Id</th>
-      <th scope="col">Vardas Pavardė</th>
-      <th scope="col">Projektas</th>
-      <th scope="col">Veiksmas</th>
-    </tr>
-  </thead>
+    <thead style="background-color: lightblue">
+      <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Vardas Pavardė</th>
+        <th scope="col">Projektas</th>
+        <th scope="col">Veiksmas</th>
+      </tr>
+    </thead>
 
   <tbody>
     <tr>
       <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+        <td>Petras Petraitis</td>
+        <td>Personalo skyrius</td>
+      <td>               
+         <form action="" method="post">
+         <button type="button" name="submit" class="btn btn-primary btn-sm" style="background-color: lightblue; color:black; border-color:black">Ištrinti</button>
+         <button type="button" name="submit" class="btn btn-primary btn-sm" style="background-color: lightblue; color:black; border-color:black">Atnaujinti</button>
+          </form>
+      </td>
     </tr>
-    <tr>
   </tbody>
 </table>
 
-<div class="container">
-    <div class="row">
-      <div class="col-3">
-          <form action="" method="post">    
-            <input type="name" name="name" class="form-control" id="name" placeholder="Naujas projekto pavadinimas">
-      </div>
-        <div class="col-2">
-            <button type="button" name="submit" class="btn btn-primary" style="background-color: lightblue; color:black; border-color:black">Pridėti projektą</button>
+    <div class="container">
+        <div class="row">
+          <div class="col-3">
+              <form action="" method="post">    
+                <input type="name" name="name" class="form-control" id="name" placeholder="Naujas projekto pavadinimas">
+          </div>
+            <div class="col-2">
+                <button type="button" name="submit" class="btn btn-primary" style="background-color: lightblue; color:black; border-color:black">Pridėti projektą</button>
+            </div>
+            </form>
         </div>
-        </form>
     </div>
-</div>
-</div>
-</div>
-</div>
+   </div>
+  </div>
+ </div>
 </body>
 </html>
