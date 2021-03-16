92ddba84f92b44b4e3c50d7ea6111a94c82834d5
InetaVei <74532995+InetaVei@users.noreply.github.com>
2021-03-16 15:41:40 +0200

    html

--- index.php
+++ index.php
@@ -8,16 +8,26 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
   
   <title>Sprint2</title>
+
 </head>
 <body>
+
   <div class="container-fluid">
     <div class="row">
       <div class="col-12">
-      
-          <div class="btn-group" role="group" aria-label="Basic example">
-            <button type="button" name="submit" class="btn btn-primary" style="background-color: lightblue; color:black; border-color:black">Darbuotojas</button>
-            <button type="button" name="submit" class="btn btn-primary" style="background-color: lightblue; color:black; border-color:black">Projektas</button>
-          </div>
+
+            <div class="card text-center">
+        <div class="card-header">
+          <ul class="nav nav-tabs card-header-tabs">
+            <li class="nav-item">
+              <a class="nav-link active" style="background-color: lightblue; color:black" aria-current="true" href="#">Darbuotojai</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" style="color:black" href="#">Projektai</a>
+            </li>
+          </ul>
+        </div>
+      </div>
 
   <table class="table table-striped table-hover">
 
@@ -31,14 +41,15 @@
     </thead>
 
   <tbody>
+
     <tr>
       <th scope="row">1</th>
         <td>Petras Petraitis</td>
         <td>Personalo skyrius</td>
       <td>               
          <form action="" method="post">
-         <button type="button" name="submit" class="btn btn-primary btn-sm" style="background-color: lightblue; color:black; border-color:black">Ištrinti</button>
-         <button type="button" name="submit" class="btn btn-primary btn-sm" style="background-color: lightblue; color:black; border-color:black">Atnaujinti</button>
+         <button type="button" name="submit" class="btn btn-primary btn-sm" style="background-color: lightblue; color:black; border-color:black">Delete</button>
+         <button type="button" name="submit" class="btn btn-primary btn-sm" style="background-color: lightblue; color:black; border-color:black">Update</button>
           </form>
       </td>
     </tr>
@@ -48,7 +59,7 @@
     <div class="container">
         <div class="row">
           <div class="col-3">
-              <form action="" method="post">    
+              <form action="" method="">    
                 <input type="name" name="name" class="form-control" id="name" placeholder="Naujas projekto pavadinimas">
           </div>
             <div class="col-2">
